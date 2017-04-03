76c75a61e43efb6beec482a73b83e6139693b811
Arbona <jeanmichel.arbona@gmail.com>
2017-04-03 12:30:16 +0200

    added modif ens

--- training/helpers.py
+++ training/helpers.py
@@ -10,6 +10,25 @@ import argparse
 chars = "ACGT"
 mapping = {"A": 0, "C": 1, "G": 2, "T": 3, "N": 4}
 
+def scale(X):
+  m25 = np.percentile(X[:,0], 25)
+  m75 = np.percentile(X[:,0], 75)
+  s50 = np.median(X[:,2])
+  me25 = 0.07499809
+  me75 = 0.26622871
+  se50 = 0.6103758
+  ret = np.array(X)
+  scale = (me75 - me25) / (m75 - m25)
+  m25 *= scale
+  shift = me25 - m25
+  ret[:,0] = X[:,0] * scale + shift
+  ret[:,1] = ret[:,0]**2
+  
+  sscale = se50 / s50
+
+  ret[:,2] = X[:,2] * sscale
+  return ret
+
 def preproc_event(mean, std, length):
   mean = mean / 100.0 - 0.66
   std = std - 1
